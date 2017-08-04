966ad9045e8f499e50f03790983050d3fef9df4d
Andor Molnár <andor.molnar@logmein.com>
2017-08-04 22:28:46 +0200

    Added first bunch of code

---
+++ src/org/dolphy/Main.java
@@ -0,0 +1,26 @@
+package org.dolphy;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Andor!");
+
+        Map<String, Integer> syncMap = Collections.synchronizedMap(new HashMap<String, Integer>());
+        Map<String, Integer> myMap = new HashMap<String, Integer>();
+
+        myMap.put("andor", 3);
+        myMap.put("bela", 5);
+
+        System.out.printf("Andor = %d\n", myMap.get("andor"));
+
+        myMap.forEach((k,v) -> {
+            System.out.printf("%s => %d\n", k, v);
+        });
+
+
+    }
+}
