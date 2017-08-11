6b25ccbe637329c54d59f058ad3e2a84237a25c4
Andor Molnár <andor.molnar@logmein.com>
2017-08-11 16:37:34 +0200

    LogWriter added

---
+++ alone/LogWriter.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.concurrent.*;
+
+public class LogWriter {
+	private final int CAPACITY = 10;
+	private final BlockingQueue<String> queue;
+	private final LoggerThread logger;
+	private boolean isShutdown;
+	private int reservations;
+
+	public LogWriter(PrintWriter writer) {
+		this.queue = new LinkedBlockingQueue<String>(CAPACITY);
+		this.logger = new LoggerThread(writer);
+	}
+
+	public void start() { logger.start(); }
+
+	public void stop() {
+		synchronized (this) {
+			isShutdown = true;
+		}
+		logger.interrupt();
+	}
+
+	public void log(String msg) throws InterruptedException {
+		synchronized (this) {
+			if (isShutdown)
+				throw new IllegalStateException();
+			++reservations;
+		}
+		System.out.println("new message: " + msg);
+		queue.put(msg);
+	}
+
+	private class LoggerThread extends Thread {
+		private final PrintWriter writer;
+
+		public LoggerThread(PrintWriter writer) {
+			this.writer = writer;
+		}
+
+		public void run() {
+			try {
+				while (true) {
+					try {
+						synchronized (LogWriter.this) {
+							if (isShutdown && reservations == 0)
+								break;
+						}
+						System.out.println("Waiting for element");
+						String msg = queue.take();
+						synchronized (LogWriter.this) {
+							--reservations;
+						}
+						writer.println(msg);
+						writer.flush();
+					} catch (InterruptedException e) { /* retry */ }
+				}	
+			} finally {
+				writer.close();
+			}			
+		}
+	}
+}
+
+
---
+++ alone/startLog.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class startLog {
+	public static void main(String[] args) {
+		try {
+			PrintWriter writer = new PrintWriter(System.out);
+			LogWriter log = new LogWriter(writer);
+			log.start();
+			log.log("Hello!");
+			System.out.println("Press any key to stop");
+			try {
+        	    System.in.read();
+	        } catch(Exception e) {
+				System.out.println("Error reading keyboard: " + e);
+			}  
+			log.stop();
+		} catch(Exception e) {
+			System.out.println("Error: " + e);
+		}
+	}
+}
