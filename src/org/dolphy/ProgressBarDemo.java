d0cd34772a276b907195b037cbf1143c5cf13080
Andor Molnár <andor.molnar@logmein.com>
2017-08-16 19:10:33 +0200

    Some progress bar fun

---
+++ src/org/dolphy/ProgressBarDemo.java
@@ -0,0 +1,133 @@
+package org.dolphy;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.beans.*;
+import java.util.Objects;
+import java.util.Random;
+
+public class ProgressBarDemo extends JPanel {
+    private JProgressBar progressBar;
+    private JButton startButton, cancelButton;
+    private JTextArea taskOutput;
+    private Task task;
+
+    class Task extends SwingWorker<Void, Void> {
+        /*
+         * Main task. Executed in background thread.
+         */
+        @Override
+        public Void doInBackground() {
+            Random random = new Random();
+            int progress = 0;
+            //Initialize progress property.
+            setProgress(0);
+            while (progress < 100) {
+                //Sleep for up to one second.
+                try {
+                    Thread.sleep(random.nextInt(1000));
+                } catch (InterruptedException ignore) {}
+                //Make random progress.
+                progress += random.nextInt(10);
+                setProgress(Math.min(progress, 100));
+            }
+            return null;
+        }
+
+        /*
+         * Executed in event dispatching thread
+         */
+        @Override
+        public void done() {
+            Toolkit.getDefaultToolkit().beep();
+            startButton.setEnabled(true);
+            setCursor(null); //turn off the wait cursor
+            taskOutput.append("Done!\n");
+        }
+
+    }
+
+    private ProgressBarDemo() {
+        super(new BorderLayout());
+
+        //Create the demo's UI.
+        startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startButton.setEnabled(false);
+//                setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                //Instances of javax.swing.SwingWorker are not reusuable, so
+                //we create new instances as needed.
+                task = new Task();
+                task.addPropertyChangeListener(new PropertyChangeListener() {
+                    @Override
+                    public void propertyChange(PropertyChangeEvent evt) {
+                        if (Objects.equals("progress", evt.getPropertyName())) {
+                            int progress = (Integer) evt.getNewValue();
+                            progressBar.setValue(progress);
+                            taskOutput.append(String.format(
+                                    "Completed %d%% of task.\n", task.getProgress()));
+                        }
+                    }
+                });
+                task.execute();
+                cancelButton.setEnabled(true);
+            }
+        });
+
+        cancelButton = new JButton("Cancel");
+        cancelButton.setEnabled(false);
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cancelButton.setEnabled(false);
+                task.cancel(true);
+                taskOutput.append("Task has been canceled");
+            }
+        });
+
+        progressBar = new JProgressBar(0, 100);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+
+        taskOutput = new JTextArea(5, 20);
+        taskOutput.setMargin(new Insets(5,5,5,5));
+        taskOutput.setEditable(false);
+
+        JPanel panel = new JPanel();
+        panel.add(startButton);
+        panel.add(cancelButton);
+        panel.add(progressBar);
+
+        add(panel, BorderLayout.PAGE_START);
+        add(new JScrollPane(taskOutput), BorderLayout.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    }
+
+    /**
+     * Create the GUI and show it. As with all GUI code, this must run
+     * on the event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("ProgressBarDemo");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        JComponent newContentPane = new ProgressBarDemo();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(ProgressBarDemo::createAndShowGUI);
+    }
+}
