6b43eca090318abbe4c4ddc74f730f270df8e6aa
Andor Molnár <andor.molnar@logmein.com>
2017-08-30 17:01:53 +0200

    Prio queue

--- src/org/dolphy/StudentPrio.java
+++ src/org/dolphy/StudentPrio.java
@@ -5,18 +5,19 @@ import java.util.*;
 /*
  * Create the Student and Priorities classes here.
  */
-class Student {
-    private final int id;
-    private final String name;
-    private final double cgpa;
 
-    public Student(int id, String name, double cgpa) {
+class Student implements Comparable<Student> {
+    private final Integer id;
+    private final String name;
+    private final Double cgpa;
+
+    public Student(int id, String name, Double cgpa) {
         this.id = id;
         this.name = name;
         this.cgpa = cgpa;
     }
 
-    public int getID() {
+    public Integer getID() {
         return this.id;
     }
 
@@ -24,16 +25,50 @@ class Student {
         return this.name;
     }
 
-    public double getCGPA() {
+    public Double getCGPA() {
         return this.cgpa;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if (o.cgpa.equals(this.cgpa)) {
+            if (o.name.equals(this.name)) {
+                return o.id.compareTo(this.id);
+            }
+            return this.name.compareTo(o.name);
+        }
+        return o.cgpa.compareTo(this.cgpa);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
     }
 }
 
 class Priorities {
-    private final Queue<Student> q = new PriorityQueue();
+    private final Queue<Student> q = new PriorityQueue<>();
 
     List<Student> getStudents(List<String> events) {
-        
+        for (String e : events) {
+            Scanner s = new Scanner(e);
+            String cmd = s.next();
+            if ("SERVED".equals(cmd)) {
+                System.out.println(q);
+                System.out.printf("Served: %s\n", q.poll());
+            }
+            if ("ENTER".equals(cmd)) {
+                String name = s.next();
+                double cgpa = s.nextDouble();
+                int id = s.nextInt();
+                q.add(new Student(id, name, cgpa));
+            }
+        }
+        List<Student> l = new ArrayList<Student>();
+        while (!q.isEmpty()) {
+            l.add(q.poll());
+        }
+        return l;
     }
 }
 
